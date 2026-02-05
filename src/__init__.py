701fef5c2aa8a9ee209272f35dfa04db48fd17db
hmaru <hmaru@digitalocean.com>
2026-02-06 02:29:31 +0530

    update exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.0"
+from .matching_engine import ResumeMatchingEngine, MatchResult, RankedResume
+from .preprocessor import TextPreprocessor
+from .feature_extractor import SkillExtractor
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "ResumeMatchingEngine",
+    "MatchResult",
+    "RankedResume",
+    "TextPreprocessor",
+    "SkillExtractor",
+]
